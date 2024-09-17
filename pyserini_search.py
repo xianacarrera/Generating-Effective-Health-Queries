2179e59437b771244351e922403311cb11a2e390
xianacarrera <xiana.carrera.alonso@gmail.com>
2024-09-17 12:45:48 +0200

    Change search word

--- pyserini_search.py
+++ pyserini_search.py
@@ -2,7 +2,7 @@ from pyserini.search import SimpleSearcher
 
 if __name__ == "__main__":
     searcher = SimpleSearcher('/mnt/hpc-irgroup/indexes/misinfo-2020')
-    hits = searcher.search('COVID')
+    hits = searcher.search('covid')
 
     print(f'Total number of hits: {len(hits)}')
 
